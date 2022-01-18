687864794739f0c4125864ed179a861b218a2f40
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-18 09:32:19 +0200

    Use state hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
     return (
         <div className="home">
-            <h2>Home page</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
-  };
-  
-  export default Home;
+};
+
+export default Home;
