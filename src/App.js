9995943da71635d719776b6ce6e781ee087da1d9
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-19 19:37:06 +0200

    Router Links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import Home from './Home';
 import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route path="/">
-                <Home/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/create">
+                            <Create />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
